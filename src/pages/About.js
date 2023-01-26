8fc09c951da52891a5c40bf104d473dfb5dd5b2b
Eric Thomas <eathom91@gmail.com>
2023-01-26 12:38:30 -0500

    setup basic routing

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <h2>Homepage</h2>
+      <h2>About</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed hendrerit ex. 
          Vivamus eget risus laoreet massa rutrum consectetur et non purus. Sed aliquam in
         magna sit amet pellentesque. Quisque non lacinia arcu, vel lacinia nisi.
