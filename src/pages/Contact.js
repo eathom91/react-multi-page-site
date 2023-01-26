84f72889f9f578c23911f24f434edd1e32777827
Eric Thomas <eathom91@gmail.com>
2023-01-26 12:27:22 -0500

    added pages dir/3 base pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed hendrerit ex. 
+         Vivamus eget risus laoreet massa rutrum consectetur et non purus. Sed aliquam in
+        magna sit amet pellentesque. Quisque non lacinia arcu, vel lacinia nisi.
+      </p>
+    </div>
+  )
+}
