19ff1209f3cee6a554890637ca8101ea24e2b377
Eric Thomas <eathom91@gmail.com>
2023-01-26 13:27:39 -0500

    fixed router err logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // page components
 import Home from './pages/Home';
@@ -13,16 +13,18 @@ function App() {
       <nav>
         <h1>My Articles</h1>
       </nav>
-
-      <Route path="/">
-        <Home/>
-      </Route>
-      <Route path="/about">
-        <About/>
-      </Route>
-      <Route path="/contact">
-        <Contact/>
-      </Route>
+      <Switch> 
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+      </Switch>
+      
     </BrowserRouter>
     </div>
   );
