5530c44d679bce7b281290aa1973292dce5e665b
Eric Thomas <eathom91@gmail.com>
2023-01-26 16:36:54 -0500

    setup dynamic routing to articles

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,22 @@
+import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
 export default function Article() {
+
+const { id } = useParams()
+const url = 'http://localhost:3000/articles/' + id
+const { data: article, isPending, error } = useFetch(url)
   return (
     <div>
-      Article page
+      {isPending && <div>Loading...</div>}
+       {error && <div>{error}</div>}
+       {article && (
+        <div key={id} className="card">
+          <h3>{article.title}</h3>
+          <h4>By {article.author}</h4>
+          <p>{article.body}</p>
+        </div>
+       )}
     </div>
   )
 }
